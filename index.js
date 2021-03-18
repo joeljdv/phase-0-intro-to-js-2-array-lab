be681035bf9d639f09d03d74b72caf3703d384ba
joeljdv <joeldiazjdv@gmail.com>
2021-03-17 21:49:21 -0400

    commit tree bcfd14176c1268dfcc095c1fc66d55e25440cd75

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  }); 
+  
+  function destructivelyAppendCat(){
+      cats.push('Ralph')
+      
+  }
+  function destructivelyPrependCat(){
+      cats.unshift('Bob')
+  }
+
+  function destructivelyRemoveLastCat(){
+      cats.pop('Ralph')
+  }
+
+  function destructivelyRemoveFirstCat(){
+      cats.shift('Bob')
+  }
+
+  function appendCat(){
+    const copyCats=[...cats,'Broom'] 
+    return copyCats
+  }
+
+  function prependCat(){
+    const copyCats2=['Arnold',...cats]
+    return copyCats2
+  }
+
+  function removeLastCat(){
+      const catsArray=cats.slice(0,2)
+      return catsArray
+  }
+  
+  function removeFirstCat(){
+      const catsArray2=cats.slice(1)
+      return catsArray2
+  }
